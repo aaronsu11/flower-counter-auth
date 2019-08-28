8d454b3aa630994b5413976384cdda048fc3a244
Aaron <aaronsu11@outlook.com>
2019-08-28 17:22:56 +1000

    test mode

--- server.js
+++ server.js
@@ -25,7 +25,8 @@ app.use(cors())
 app.use(bodyParser.json());
 
 app.get('/', (req, res) => { 
-	res.send(db.users);
+	// res.send(db.users);
+	res.send('Runing');
 })
 
 app.post('/signin', signin.handleSignin(db, bcrypt))
@@ -34,6 +35,6 @@ app.get('/profile/:id', (req, res) => { profile.handleProfileGet(req, res, db)})
 app.put('/image', (req, res) => { image.handleImage(req, res, db)})
 app.post('/imageurl', (req, res) => { image.handleApiCall(req, res)})
 
-app.listen(3000, ()=> {
-  console.log('app is running on port 3000');
+app.listen(process.env.PORT || 3000, ()=> {
+  console.log('app is running on port ${process.env.PORT}');
 })

cf3f617730082ded0dd89a381d3836547a0b4b00
Aaron <aaronsu11@outlook.com>
2019-10-02 22:40:28 +1000

    pushing to github

--- server.js
+++ server.js
@@ -9,15 +9,42 @@ const signin = require('./controllers/signin');
 const profile = require('./controllers/profile');
 const image = require('./controllers/image');
 
-const db = knex({
-  client: 'pg',
-  connection: {
-  	connectionString : process.env.DATABASE_URL,
-  	ssl: true
-  }
-});
+// const db = knex({
+//   client: 'pg',
+//   connection: {
+//   	connectionString : process.env.DATABASE_URL,
+//   	ssl: true
+//   }
+// });
 
 const app = express();
+
+const connect = () => {
+  // [START gae_flex_postgres_connect]
+  const config = {
+    user: process.env.SQL_USER,
+    password: process.env.SQL_PASSWORD,
+    database: process.env.SQL_DATABASE,
+  };
+
+  if (
+    process.env.INSTANCE_CONNECTION_NAME &&
+    process.env.NODE_ENV === 'production'
+  ) {
+    config.host = `/cloudsql/${process.env.INSTANCE_CONNECTION_NAME}`;
+  }
+
+  // Connect to the database
+  const db = knex({
+    client: 'pg',
+    connection: config,
+  });
+  // [END gae_flex_postgres_connect]
+
+  return db;
+};
+
+const db = connect();
 
 app.use(cors())
 app.use(bodyParser.json());
@@ -33,6 +60,6 @@ app.get('/profile/:id', (req, res) => { profile.handleProfileGet(req, res, db)})
 app.put('/image', (req, res) => { image.handleImage(req, res, db)})
 app.post('/imageurl', (req, res) => { image.handleApiCall(req, res)})
 
-app.listen(process.env.PORT || 3000, ()=> {
+app.listen(process.env.PORT || 8080, ()=> {
   console.log('app is running on port ${process.env.PORT}');
 })
